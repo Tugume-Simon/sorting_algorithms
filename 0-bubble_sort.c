9b86283ad9fe583f50725cbf3596e6fae7032460
tugume-peter <simontugume2@gmail.com>
2023-02-19 03:32:09 -0800

    bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts a given array using the bubble sort algorithm
+ *
+ * @array: the array
+ * @size: number of elements in the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int curr, next;
+	int flag;
+
+	for (i = 0; i < size; i++)
+	{
+		flag = 1;
+		for (j = 0; j < size; j++)
+		{
+			curr = array[j];
+			next = array[j + 1];
+			if (next < curr)
+			{
+				array[j] = next;
+				array[j + 1] = curr;
+				flag = 0;
+				print_array(array, size);
+			}
+		}
+		if (flag == 1)
+			break;
+	}
+}
--- sort.h
+++ sort.h
@@ -2,48 +2,6 @@
 #define SORT_H
 
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-	size_t i;
-
-	i = 0;
-	while (array && i < size)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i]);
-		++i;
-	}
-	printf("\n");
-}
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list)
-{
-	int i;
-
-	i = 0;
-	while (list)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", list->n);
-		++i;
-		list = list->next;
-	}
-	printf("\n");
-}
-
-/**
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
@@ -56,5 +14,10 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
+/* prototypes */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
 #endif /* SORT_H */
